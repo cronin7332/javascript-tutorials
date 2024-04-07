35f37d20c8edd7fb0881f2bf9f75f880c4719d29
Chandrakant Rohit <126598304+cronin7332@users.noreply.github.com>
2024-04-07 07:21:01 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict" // treat all JS code as newer version
+
+// alert( 3 + 3 ); // will give an error because we are using Nodejs and not the Browser
+
+
+let name = "Chandrakant Rohit"; // String
+let age = "22"; // Number
+let isLoggedIn = True; // Boolean
+// Null // Stand alone Value
+// Undefined // Not defined Value
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
