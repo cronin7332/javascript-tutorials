a42a33547012f2f5f1a56c2ac8b97823ef72dc89
Chandrakant Rohit <126598304+cronin7332@users.noreply.github.com>
2024-04-16 08:50:57 +0000

    compare

---
+++ 01_Basics/04_comparisions.js
@@ -0,0 +1,23 @@
+// Basic comparisions
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === is a special operator because it compare value and data type both
+
+console.log("2" === 2); // returns False due to change in data type
