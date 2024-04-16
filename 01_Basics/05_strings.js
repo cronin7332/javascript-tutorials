7e0f386ee9bf4b56f5e47e3c260c1533a959ea6d
Chandrakant Rohit <126598304+cronin7332@users.noreply.github.com>
2024-04-16 08:54:34 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,35 @@
+const username = "rohit"
+const repoCount = 20
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${username} and my repo count is ${repoCount}`);
+
+const gameName = new String('rohit-cr-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://rohit.com/rohit%20chandrakant"
+
+console.log(url.replace('%20', '-')) // returns https://rohit.com/rohit-chandrakant
+
+console.log(url.includes('sundar')) // False
+
+console.log(gameName.split('-'));
